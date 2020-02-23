5fae826f9142bbf599e14c586ec480c174ef87b2
gwegwe1234 <gwangtae5860@daum.net>
2020-02-23 22:19:40 +0900

    Initial Commit

---
+++ src/main/java/blog/config/RootConfig.java
@@ -0,0 +1,13 @@
+package blog.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = {"blog"}, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
+public class RootConfig {
+
+}
---
+++ src/main/java/blog/config/SpringWebInitializer.java
@@ -0,0 +1,22 @@
+package blog.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected String[] getServletMappings() {
+        //DispatcherServlet을 /에 매핑
+        return new String[] {"/"};
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        // 설정 클래스를 지정해 준다.
+        return new Class<?>[] {WebConfig.class};
+    }
+}
